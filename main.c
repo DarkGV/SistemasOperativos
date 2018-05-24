e37d1def1d33fbcd5be9703bb9d49bcc4363297a
DarkGV <almirante.silva.96@gmail.com>
2018-05-24 23:05:32 +0100

    Fixed some bugs

---
+++ main.c
@@ -0,0 +1,20 @@
+/*
+*Aplicação principal: 
+* Thread para criar a ponte entre o client e o servidor 
+*   (Aplicação principal é o sv)
+*
+*/
+
+
+#include "link.h"
+
+int main()
+{
+    pthread_t thread;
+    pthread_create(&thread, NULL, create_listener, NULL);
+    while(1){
+        if(received){
+            received = 0;
+        }
+    }
+}
