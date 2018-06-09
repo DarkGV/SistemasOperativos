bc6af00123e9ac83e34b701932c9e1c397184204
Diogo Silva <almirante.silva.96@gmail.com>
2018-06-09 11:36:25 +0100

    Updated

--- teste.c
+++ teste.c
@@ -2,20 +2,20 @@
 #include "clientAPI.h"
 
 int main(){
-    //ISTO INICIA UMA SESSÃO COM O SERVIDOR
+    /*ISTO INICIA UMA SESSÃO COM O SERVIDOR*/
     int s = startCommunication("192.168.207.199");
-                                //IP DO SERVIDOR
+                                /*IP DO SERVIDOR*/
     if(s < 0){
-        //ISTO É A MENSAGEM DE ERRO PARA CASO DÊ 
-        //ERRO AO LIGAR AO SERVIDOR
+        /*ISTO É A MENSAGEM DE ERRO PARA CASO DÊ*/
+        /*ERRO AO LIGAR AO SERVIDOR*/
         printf("Erro ao conectar com o servidor");
         return -1;
     }
-    //ISTO CRIA O FICHEIRO .log
+    /*ISTO CRIA O FICHEIRO .log*/
     createLogFile("logFileSend.log", "HELP! I NEED SOMEBODY", 4);
 
-    //ISTO ENVIA O FICHEIRO .log
-    if(sendFileTo(s, "main.c") < 0){
+    /*ISTO ENVIA O FICHEIRO .log*/
+    if(sendFileTo(s, "logFileSend.log") < 0){
         printf("Erro ao enviar ficheiro");
     }
 
